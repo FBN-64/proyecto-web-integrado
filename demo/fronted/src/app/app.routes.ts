55b5caecb978af073e3d58b83f02ca702241762f
FBN-64 <falonso2024@gmail.com>
2026-06-29 14:28:34 -0500

    Rutas anidadas
    -  Se agregó a configuración las rutas hijas de perfil y seguridad

--- demo/fronted/src/app/app.routes.ts
+++ demo/fronted/src/app/app.routes.ts
@@ -13,6 +13,8 @@ import { MensajesComponent } from './pages/mensajes/Mensajes';
 import { ConfiguracionComponent } from './pages/configuracion/configuracion';
 import { authGuard } from './guards/auth.guard';
 import { Pagina404 } from './pages/pagina404/pagina404';
+import { Perfil } from './pages/configuracion/perfil/perfil';
+import { Seguridad } from './pages/configuracion/seguridad/seguridad';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -32,7 +34,15 @@ export const routes: Routes = [
       { path: 'pacientes', component: PacientesComponent },
       { path: 'pagos', component: PagosComponent },
       { path: 'mensajes', component: MensajesComponent },
-      { path: 'configuracion', component: ConfiguracionComponent }
+      {
+        path: 'configuracion',
+        component: ConfiguracionComponent,
+        children: [
+          { path: '', redirectTo: 'perfil', pathMatch: 'full' },
+          { path: 'perfil', component: Perfil },
+          { path: 'seguridad', component: Seguridad }
+        ]
+      }
     ]
   },
   { path: '**', component: Pagina404 }
--- demo/fronted/src/app/pages/configuracion/configuracion.ts
+++ demo/fronted/src/app/pages/configuracion/configuracion.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { RouterOutlet, RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-configuracion',
-  imports: [],
+  imports: [RouterOutlet, RouterLink, RouterLinkActive],
   templateUrl: './configuracion.html',
   styleUrl: './configuracion.css',
 })
