64513a2ceb5151319afe9d29967d3f5cfb9dbe1e
FBN-64 <falonso2024@gmail.com>
2026-07-15 15:03:10 -0500

    Actualizado
    - Eliminado configuración y ahora ya se actualiza la busqueda

--- demo/fronted/src/app/app.routes.ts
+++ demo/fronted/src/app/app.routes.ts
@@ -10,11 +10,8 @@ import { CitasComponent } from './pages/citas/citas';
 import { PacientesComponent } from './pages/pacientes/Pacientes';
 import { PagosComponent } from './pages/pagos/Pagos';
 import { MensajesComponent } from './pages/mensajes/Mensajes';
-import { ConfiguracionComponent } from './pages/configuracion/configuracion';
 import { authGuard } from './guards/auth.guard';
 import { Pagina404 } from './pages/pagina404/pagina404';
-import { Perfil } from './pages/configuracion/perfil/perfil';
-import { Seguridad } from './pages/configuracion/seguridad/seguridad';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'login', pathMatch: 'full' },
@@ -34,15 +31,6 @@ export const routes: Routes = [
       { path: 'pacientes', component: PacientesComponent },
       { path: 'pagos', component: PagosComponent },
       { path: 'mensajes', component: MensajesComponent },
-      {
-        path: 'configuracion',
-        component: ConfiguracionComponent,
-        children: [
-          { path: '', redirectTo: 'perfil', pathMatch: 'full' },
-          { path: 'perfil', component: Perfil },
-          { path: 'seguridad', component: Seguridad }
-        ]
-      }
     ]
   },
   { path: '**', component: Pagina404 }
