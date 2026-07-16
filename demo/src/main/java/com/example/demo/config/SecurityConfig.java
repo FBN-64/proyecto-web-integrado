f1cab81a79a2e96cb0a5a54fde26e130db25d013
FBN-64 <falonso2024@gmail.com>
2026-07-15 23:12:35 -0500

    Config
    - Se Arregló el posible error a la hora de desplegar en vercel

--- demo/src/main/java/com/example/demo/config/SecurityConfig.java
+++ demo/src/main/java/com/example/demo/config/SecurityConfig.java
@@ -2,6 +2,8 @@ package com.example.demo.config;
 
 import com.example.demo.repository.UsuarioRepository;
 import java.util.List;
+
+import org.springframework.web.filter.CorsFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -37,6 +39,23 @@ public class SecurityConfig {
                 .formLogin(form -> form.disable())
                 .httpBasic(basic -> basic.disable())
                 .build();
+    }
+
+    @Bean
+    public CorsFilter corsFilter() {
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        CorsConfiguration config = new CorsConfiguration();
+        
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(List.of(
+            "http://localhost:4200", 
+            "https://madre-zoraida-admin-nine.vercel.app"
+        ));
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS"));
+        config.setAllowedHeaders(List.of("*"));
+        
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
     }
 
     @Bean
